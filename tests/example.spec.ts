1a569255224ae40a477b5fb7a10ecd3f0d8d749e
Ocean <ocean@MacBook-Air-10.local>
2025-09-21 14:26:54 +0100

    test: playwright test on the game flow

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,40 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("game logic", async ({ page }) => {
+  await page.goto("https://thryve-wheat.vercel.app/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await page.getByRole("textbox", { name: "Please enter your name" }).click();
+  await page
+    .getByRole("textbox", { name: "Please enter your name" })
+    .fill("Li");
+  await expect(page.getByText("Name must be at least 4")).toBeVisible();
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page
+    .getByRole("textbox", { name: "Please enter your name" })
+    .fill("Lik");
+  await page.getByRole("button", { name: "Start game" }).click();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByText("Lik").click();
+  await page.getByText("B", { exact: true }).click();
+  await page.getByText("Correct: 0 · Errors: 0 ·").click();
+  await page.getByText("D", { exact: true }).click();
+  await page.getByRole("button", { name: "Match letters" }).click();
+  await page
+    .getByRole("alert")
+    .filter({ hasText: "😔 Didn't match" })
+    .isVisible();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.getByRole("button", { name: "Match letters" }).click();
+  await page
+    .getByRole("alert")
+    .filter({ hasText: "😍 You have a match" })
+    .isVisible();
+
+  await page.getByText("✅ Correct: 2❌ Errors:").isVisible();
+
+  await page.getByRole("button", { name: "Play again" }).click();
+  await page.getByRole("button", { name: "Toggle theme" }).click();
+  await page.getByRole("menuitem", { name: "Light" }).click();
+  await page.getByRole("button", { name: "Toggle theme" }).click();
+  await page.getByRole("menuitem", { name: "Dark" }).click();
 });
